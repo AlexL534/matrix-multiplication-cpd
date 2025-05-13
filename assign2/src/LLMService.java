4ac9ee0f36bb5030b01aed205dc23e8a5ba355c6
Alexandre Lopes <alexandrefernadeslopes@gmail.com>
2025-05-13 23:10:26 +0100

    small change

--- assign2/src/LLMService.java
+++ assign2/src/LLMService.java
@@ -67,7 +67,6 @@ public class LLMService {
                 while ((line = in.readLine()) != null) {
                     response.append(line);
                 }
-                System.out.println("Raw response: " + response);
                 return parseAIResponse(response.toString());
             }
         } finally {
