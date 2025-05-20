c83936cbab4826aca38247ab3b38ce14310d29a2
Lucas Faria <lucasfaria@outlook.pt>
2025-05-20 14:48:39 +0100

    Small changes

--- assign2/src/ChatService.java
+++ assign2/src/ChatService.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
